f5e99e5a41724e523f71e3f743328f6f184600b4
Dev-Edsonv <edsonvieiradeveloper@gmail.com>
2024-11-03 00:36:42 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let firstNumber = document.querySelector("#firstNumber").value
 let secoudNumber = document.querySelector("#secoundNumber).value
 let paragraph = document.querySelector("p")                                        
 
-  if(firstNumber.value >= secoundNumber) {
+  if(firstNumber >= secoundNumber) {
     paragraph.innerHTML = " O primeiro número não poder ser maior/igual ao segundo"
 
 }else {
