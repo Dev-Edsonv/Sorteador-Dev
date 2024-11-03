b8b498a25b0b6ca7827bf839ae94db579c83174e
Dev-Edsonv <edsonvieiradeveloper@gmail.com>
2024-11-03 20:56:39 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
-function draw() {
-
 let firstNumber = document.querySelector("#firstNumber").value
 let secoudNumber = document.querySelector("#secoundNumber).value
 let paragraph = document.querySelector("p")                                        
+
+function draw() {
 
   if(firstNumber >= secoundNumber) {
     paragraph.innerHTML = " O primeiro número não poder ser maior/igual ao segundo"
 
 }else {
     let drawNumber = Math.floor(Math.random() * (firstNumber - secoundNumber + 1)) + secoundNumber;
+    alert('o numero sorteado foi ${drawNumber}')
   }
