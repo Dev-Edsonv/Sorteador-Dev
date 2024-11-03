6768571d212f15ab2a93856f0722299219cd6b7c
Dev-Edsonv <edsonvieiradeveloper@gmail.com>
2024-11-03 00:25:22 -0300

    Parte Externa pronta.
    Parte externa do site já pronta.

---
+++ script.js
@@ -0,0 +1,12 @@
+function draw() {
+
+let firstNumber = document.querySelector("#firstNumber").value
+let secoudNumber = document.querySelector("#secoundNumber).value
+let paragraph = document.querySelector("p")                                        
+
+  if(firstNumber.value >= secoundNumber) {
+    paragraph.innerHTML = " O primeiro número não poder ser maior/igual ao segundo"
+
+}else {
+    let drawNumber = Math.floor(Math.random() * (firstNumber - secoundNumber + 1)) + secoundNumber;
+  }
