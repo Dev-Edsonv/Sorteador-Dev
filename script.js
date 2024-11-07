c6807b921c348b7e84be3f7b843de6bdcec1456e
Dev-Edsonv <edsonvieiradeveloper@gmail.com>
2024-11-06 22:35:45 -0300

    concertando erro do site

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function drawNumber() {
   let min = Math.ceil(document.querySelector("#firstNumber").value)
   let max = Math.floor(document.querySelector("#secoundNumber").value)
   let paragraph = document.querySelector("p")
-  let prizeDraw = Math.floor(Math.random() * (max - min) + min);
+  let prizeDraw = Math.floor(Math.random() * (max - min +1) + min);
 
   if(min >= max) {
     paragraph.innerHTML = `O primeiro número não pode ser maior/igual
